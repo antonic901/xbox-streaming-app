aec2b236e61d63268c323cffdf4d3bf8feac9be3
antonic901 <antonicnikola6@gmail.com>
2022-04-13 21:00:11 +0200

    PC: removed unused variables

--- backend-for-pc/xbox-classic-streamer/frontend/components/Modals/SettingsModal/Tabs/BasicSettings.vue
+++ backend-for-pc/xbox-classic-streamer/frontend/components/Modals/SettingsModal/Tabs/BasicSettings.vue
@@ -3,28 +3,28 @@
         <v-subheader>Here you can change some PC-specific settings</v-subheader>
         <v-list-item>
             <v-text-field
-                v-model="XBOX_IP_ADDRESS"
+                v-model="HOST_IP_ADDRESS"
                 class="mb-2"
                 outlined
                 dense
                 type="text"
                 hide-details
                 label="IP Address"
-                :rules="rulesXboxIpAddress"
+                :rules="rulesHostIpAddress"
                 readonly
             >
             </v-text-field>
         </v-list-item>
         <v-list-item>
             <v-text-field
-                v-model="XBOX_PORT"
+                v-model="HOST_PORT"
                 class="mb-2"
                 outlined
                 dense
                 type="number"
                 hide-details
                 label="Port"
-                :rules="rulesXboxPort"
+                :rules="rulesHostPort"
                 readonly
             >
             </v-text-field>
@@ -46,23 +46,18 @@
 <script>
 module.exports = {
     name: 'BasicSettings',
-    computed: {
-        XBOX_ADDRESS() {
-            return this.$store.getters.getXboxAddress;
-        }
-    },
     data() {
         return {
-            XBOX_IP_ADDRESS: null,
-            rulesXboxIpAddress: [
+            HOST_IP_ADDRESS: null,
+            rulesHostIpAddress: [
                 value => !!value || 'Requried.',
                 value => {
                     const pattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
                     return pattern.test(value) || 'IP Address is not valid.'
                 }
             ],
-            XBOX_PORT: null,
-            rulesXboxPort: [
+            HOST_PORT: null,
+            rulesHostPort: [
                 value => !!value || 'Requried.',
                 value => {
                     const pattern = /^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/
@@ -78,8 +73,8 @@ module.exports = {
         }
     },
     mounted() {
-        this.XBOX_IP_ADDRESS = window.location.hostname;
-        this.XBOX_PORT = window.location.port;
+        this.HOST_IP_ADDRESS = window.location.hostname;
+        this.HOST_PORT = window.location.port;
     }
 }
 </script>
